024bf5904e9ab3c6e1e4c69466232999af654c89
Cevin Millstead <cevinm@gmail.com>
2023-07-22 21:53:47 -0700

    3. testing contracts

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,6 +4,20 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Token {
- 	string public name = "My Token";
-
+	string public name;
+	string public symbol;
+	uint public decimals = 18;
+	uint public totalSupply; // 1 million x 10^18
+	
+	constructor(
+		string memory _name, 
+		string memory _symbol, 
+		uint _decimals, 
+		uint _totalSupply
+	) {
+		name = _name;
+		symbol = _symbol;
+		decimals = _decimals;
+		totalSupply = _totalSupply * (10**decimals);
+	}	 
 }
