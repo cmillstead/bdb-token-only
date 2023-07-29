a7a22e65ea6776e444c8f06159079a24adea8417
Cevin Millstead <cevinm@gmail.com>
2023-07-29 10:57:43 -0700

    change network to sepolia

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
+const { PRIVATE_KEY } = process.env;
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.18",
   networks: {
-    localhost: {}
+    hardhat: {},
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
   }
 };
